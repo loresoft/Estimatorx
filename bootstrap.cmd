685c4b6e52f4993240e6bb3caab7f45ebdd942c3
Paul Welter <pwelter@loresoft.com>
2015-03-23 17:17:03 -0500

    fix build bootstrap.cmd

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -2,4 +2,4 @@
 Nuget.exe restore "Code\Source\Estimatorx.sln"
 
 NuGet.exe install MSBuildTasks -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
-NuGet.exe install xunit.runners -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
+NuGet.exe install xunit.runner.console -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
