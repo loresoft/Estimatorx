53e72e0ddc2491bbfb18e8a7a979a568a1cb0660
Paul Welter <pwelter@loresoft.com>
2016-08-26 13:57:10 -0500

    add user delete

---
+++ Code/Source/Estimatorx.Core/Security/RoleNames.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Estimatorx.Core.Security
+{
+    public static class RoleNames
+    {
+        public const string Administrators = "administrators";
+        public const string Users = "users";
+    }
+}
