39d23e44747a2ca7ffdd552e7e85eea5d3dac0c6
Paul Welter <pwelter@loresoft.com>
2020-10-02 17:08:38 -0500

    cleanup

--- local-setup.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-SET NEWLINE=^& echo.
-
-FIND /C /I "local.estimatorx.com" %WINDIR%\system32\drivers\etc\hosts
-IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^127.0.0.1 local.estimatorx.com>>%WINDIR%\System32\drivers\etc\hosts
