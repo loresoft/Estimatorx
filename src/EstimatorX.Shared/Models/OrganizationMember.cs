f2fd40b8194686f7f65e2abbbe4de1ede99f8d37
Paul Welter <pwelter@loresoft.com>
2022-01-23 12:04:48 -0600

    add email to org member

--- src/EstimatorX.Shared/Models/OrganizationMember.cs
+++ src/EstimatorX.Shared/Models/OrganizationMember.cs
@@ -2,10 +2,12 @@ namespace EstimatorX.Shared.Models;
 
 public class OrganizationMember : IdentifierName
 {
+    public string Email { get; set; }
+
     public bool IsOwner { get; set; }
 
     public override int GetHashCode()
     {
-        return HashCode.Combine(base.GetHashCode(), IsOwner);
+        return HashCode.Combine(base.GetHashCode(), Email, IsOwner);
     }
 }
