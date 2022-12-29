7ebb1d3e3e733fdbcc55da96916dbc12bd42a9f4
Paul Welter <pwelter@loresoft.com>
2022-12-29 10:38:30 -0600

    update startup

--- src/EstimatorX.Service/Program.cs
+++ src/EstimatorX.Service/Program.cs
@@ -1,3 +1,18 @@
+using EstimatorX.Core.Options;
+using EstimatorX.Service.Middleware;
+using EstimatorX.Shared;
+
+using FluentValidation.AspNetCore;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Options;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+
+using SendGrid.Extensions.DependencyInjection;
+
 using Serilog;
 using Serilog.Events;
 
@@ -16,7 +31,7 @@ public static class Program
             .Enrich.FromLogContext()
             .WriteTo.Console(outputTemplate: "{Timestamp:HH:mm:ss.fff} [{Level:u1}] {Message:lj}{NewLine}{Exception}")
             .WriteTo.File(
-                path: $"{homeDirectory}/LogFiles/EstimatorX/boot.txt",
+                path: $"{homeDirectory}/LogFiles/boot.txt",
                 rollingInterval: RollingInterval.Day,
                 shared: true,
                 flushToDiskInterval: TimeSpan.FromSeconds(1),
@@ -29,17 +44,22 @@ public static class Program
         {
             Log.Information("Starting web host");
 
-            var host = Host
-                .CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
-                .UseSerilog((context, services, configuration) => configuration
-                    .ReadFrom.Configuration(context.Configuration)
-                    .ReadFrom.Services(services)
-                    .Enrich.FromLogContext()
-                )
-                .Build();
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Host.UseSerilog((context, services, configure) => configure
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext()
+            );
 
-            host.Run();
+            var services = builder.Services;
+            var configuration = builder.Configuration;
+            ConfigureServices(services, configuration);
+
+            var app = builder.Build();
+            ConfigureMiddleware(app);
+
+            app.Run();
+
 
             return 0;
         }
@@ -52,5 +72,87 @@ public static class Program
         {
             Log.CloseAndFlush();
         }
+    }
+
+
+    private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMemoryCache();
+        services.AddCosmosRepository();
+
+        services.AddEstimatorXCore();
+        services.AddEstimatorXShared();
+        services.AddEstimatorXService();
+
+        services.AddSendGrid((serviceProvider, options) =>
+        {
+            var sendGridOptions = serviceProvider.GetRequiredService<IOptions<SendGridConfiguration>>();
+            options.ApiKey = sendGridOptions.Value.ApiKey ?? "***";
+        });
+
+        services.AddAutoMapper(typeof(HostingConfiguration).Assembly, typeof(AssemblyMetadata).Assembly);
+
+        services
+            .AddOptions<HostingConfiguration>()
+            .Configure<IConfiguration>((settings, config) => config
+                .GetSection(HostingConfiguration.ConfigurationName)
+                .Bind(settings)
+            );
+
+        services
+            .AddOptions<SendGridConfiguration>()
+            .Configure<IConfiguration>((settings, config) => config
+                .GetSection(SendGridConfiguration.ConfigurationName)
+                .Bind(settings)
+        );
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApi(configuration.GetSection("AzureAd"));
+
+        services
+            .AddControllers();
+
+        services.AddFluentValidationAutoValidation();
+        services.AddFluentValidationClientsideAdapters();
+
+        services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "EstimatorX", Version = "v1" }));
+
+        services.AddResponseCompression(options =>
+        {
+            options.EnableForHttps = true;
+            ResponseCompressionDefaults.MimeTypes.Concat(new[] { "image/svg+xml" });
+        });
+
+        services.Configure<ApiBehaviorOptions>(apiBehaviorOptions => apiBehaviorOptions.SuppressModelStateInvalidFilter = true);
+    }
+
+    private static void ConfigureMiddleware(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseHsts();
+        }
+
+        // TODO, use new exception handling
+        app.UseMiddleware<JsonExceptionMiddleware>();
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EstimatorX v1"));
+
+        app.UseHttpsRedirection();
+
+        app.UseResponseCompression();
+
+        app.UseBlazorFrameworkFiles();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapControllers();
+        app.MapFallbackToFile("index.html");
     }
 }
